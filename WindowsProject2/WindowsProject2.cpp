8fda684f9098a76c09ea00d47099dbd652938fd0
x500x <109746464+x500x@users.noreply.github.com>
2024-08-05 14:47:54 +0800

    Add files via upload

--- WindowsProject2/WindowsProject2.cpp
+++ WindowsProject2/WindowsProject2.cpp
@@ -1,11 +1,14 @@
 ﻿#include <windows.h>
 #include <wininet.h>
 #include <stdio.h>
-#pragma comment(lib,"wininet.lib")   
+#pragma comment(lib,"wininet.lib")
 //#define TRANSFER_SIZE    1024
 int main() {
-bool bResult;
-    char headers[] = "Content-Type: application/x-www-form-urlencoded\r\nAuthorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjMzNzU5NjcsImlhdCI6MTcyMjc3MTE2NywiaWQiOjE4MzczNzg4NTcsIm1haWwiOiIiLCJuaWNrbmFtZSI6IjE4OTcyOTA4NjE3Iiwic3VwcGVyIjpmYWxzZSwidXNlcm5hbWUiOjE4OTcyOTA4NjE3LCJ2IjowfQ.taBpf9iV0FQikSPk6594pWT444HMAMQn4nXjPkZcg6M\r\nApp-Version: 3\r\nplatform: web";
+    bool bResult;
+    char headers[] = "Content-Type: application/x-www-form-urlencoded\r\nAuthorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjMzNzU5NjcsImlhdCI6MTcyMjc3MTE2NywiaWQiOjE4MzczNzg4NTcsIm1haWwiOiIiLCJuaWNrbmFtZSI6IjE4OTcyOTA4NjE3Iiwic3VwcGVyIjpmYWxzZSwidXNlcm5hbWUiOjE4OTcyOTA4NjE3LCJ2IjowfQ.taBpf9iV0FQikSPk6594pWT444HMAMQn4nXjPkZcg6M\r\nApp-Version: 3\r\nplatform: web\r\n\0";
+
+
+
     char data[] = "driveId=0&etag=d41d8cd98f00b204e9800998ecf8427e&fileName=txt1&parentFileId=0&size=0&type=0";
 
     HINTERNET hInternet = InternetOpenA("Mozilla/5.0 (Linux; Android 12; JLH-AN00 Build/HONORJLH-AN00) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.105 Mobile Safari/537.36", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
@@ -30,16 +33,29 @@ bool bResult;
     if( ! hRequest ) {
         goto GOTO_EXIT;
     }
-    DWORD dwFlags; 
-//dwBuffLen = sizeof(DWORD);
-    InternetQueryOptionA (hRequest, INTERNET_OPTION_SECURITY_FLAGS, (LPVOID)&dwFlags, NULL);
-    dwFlags |= SECURITY_FLAG_IGNORE_UNKNOWN_CA;
-    InternetSetOption (hRequest, INTERNET_OPTION_SECURITY_FLAGS, &dwFlags, sizeof (dwFlags) );
-    bResult = HttpSendRequestA(hRequest, headers, sizeof(headers), data, sizeof(data));
-    if ( bResult ||GetLastError() != ERROR_INTERNET_INVALID_CA )
+    /*
+        HttpAddRequestHeadersA(hRequest,headers,-1L,HTTP_ADDREQ_FLAG_ADD_IF_NEW);
+
+    */
+    /*
+        DWORD dwFlags;
+    //dwBuffLen = sizeof(DWORD);
+
+        InternetQueryOptionA (hRequest, INTERNET_OPTION_SECURITY_FLAGS, (LPVOID)&dwFlags, NULL);
+        dwFlags |= SECURITY_FLAG_IGNORE_UNKNOWN_CA;
+        InternetSetOption (hRequest, INTERNET_OPTION_SECURITY_FLAGS, &dwFlags, sizeof (dwFlags) );
+    */
+    bResult = HttpSendRequestA(hRequest, headers, -1L, data, -1L);
+    if ( !bResult ||GetLastError() != ERROR_INTERNET_INVALID_CA )
     {
         printf("errSend  errcode:%d\n",GetLastError());
+        goto GOTO_EXIT;
     }
+
+
+    DWORD statusCode;
+    HttpQueryInfoA(hConnect, HTTP_QUERY_STATUS_CODE | HTTP_QUERY_FLAG_NUMBER, &statusCode, NULL, NULL);
+    printf("Status Code: %lu\n", statusCode);
     //获得HTTP Response Header信息
     char szBuff[1024];
     //DWORD dwReadSize = 2048;
@@ -47,32 +63,29 @@ bool bResult;
     if( ! bResult ) {
         goto GOTO_EXIT;
     }
-    szBuff[1023] = '\0';
-    printf("%s/n", szBuff);
+    szBuff[1025] = '\0';
+    printf("%s\n", szBuff);
     //HTTP Response 的 Body
     DWORD dwBytesAvailable;
     bResult = InternetQueryDataAvailable(hRequest, &dwBytesAvailable, 0, 0);
     if( ! bResult ) {
         goto GOTO_EXIT;
     }
-/*
+
     if( dwBytesAvailable > TRANSFER_SIZE )
     {
         printf("tool long %d \n", GetLastError());
         goto GOTO_EXIT;
     }
-*/
+
     DWORD dwBytesRead;
     bResult = InternetReadFile(hRequest, szBuff, dwBytesAvailable, &dwBytesRead);
     if( ! bResult ) {
         goto GOTO_EXIT;
     }
-    szBuff[1023] = '\0';
-    printf("%s/n", szBuff);
+    szBuff[1025] = '\0';
+    printf("%s\n", szBuff);
 
-    DWORD statusCode;
-    HttpQueryInfoA(hConnect, HTTP_QUERY_STATUS_CODE | HTTP_QUERY_FLAG_NUMBER, &statusCode, NULL, NULL);
-    printf("Status Code: %lu\n", statusCode);
 
 GOTO_EXIT:
     if( hRequest ) {
