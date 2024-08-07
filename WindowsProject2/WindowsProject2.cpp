6642c6e8c71317514897cf73685e51677ea67371
x500x <109746464+x500x@users.noreply.github.com>
2024-08-07 17:52:26 +0800

    Add files via upload

--- WindowsProject2/WindowsProject2.cpp
+++ WindowsProject2/WindowsProject2.cpp
@@ -3,33 +3,55 @@
 #include "curl/curl.h"
 #pragma comment (lib,"libcurl.lib")
 
+CURL *hnd=NULL;
+size_t item=0;
+
+struct curl_slist * SetNormalHeaders(){
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, "User-Agent: Mozilla/5.0 (Linux; Android 12; JLH-AN00 Build/HONORJLH-AN00) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.105 Mobile Safari/537.36");
+    headers = curl_slist_append(headers, "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjMzNzU5NjcsImlhdCI6MTcyMjc3MTE2NywiaWQiOjE4MzczNzg4NTcsIm1haWwiOiIiLCJuaWNrbmFtZSI6IjE4OTcyOTA4NjE3Iiwic3VwcGVyIjpmYWxzZSwidXNlcm5hbWUiOjE4OTcyOTA4NjE3LCJ2IjowfQ.taBpf9iV0FQikSPk6594pWT444HMAMQn4nXjPkZcg6M");
+    headers = curl_slist_append(headers, "App-Version: 3");
+    headers = curl_slist_append(headers, "platform: web");
+    headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded");
+    return headers;
+}
+
+
+size_t writeCallback(char *b, size_t size, size_t nitems, void *p)
+{
+    CopyMemory(str+item,b,nitems);
+    item=1+item+nitems;
+    return nitems;
+}
 
 int main() {
-    printf("started\n");
-    CURL *hnd = curl_easy_init();
+    char *str=(char *)malloc(1024*1024);
+    hnd = curl_easy_init();
+    //重要!禁用ssl证书检查
     curl_easy_setopt(hnd, CURLOPT_SSL_VERIFYPEER, 0);
     curl_easy_setopt(hnd, CURLOPT_SSL_VERIFYHOST, 0);
+
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, NULL);
+
 
     curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "POST");
     curl_easy_setopt(hnd, CURLOPT_URL, "https://www.123pan.com/b/api/file/upload_request");
 
-    struct curl_slist *headers = NULL;
-    headers = curl_slist_append(headers, "User-Agent: Mozilla/5.0 (Linux; Android 12; JLH-AN00 Build/HONORJLH-AN00) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.105 Mobile Safari/537.36");
-    headers = curl_slist_append(headers, "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjMzNzU5NjcsImlhdCI6MTcyMjc3MTE2NywiaWQiOjE4MzczNzg4NTcsIm1haWwiOiIiLCJuaWNrbmFtZSI6IjE4OTcyOTA4NjE3Iiwic3VwcGVyIjpmYWxzZSwidXNlcm5hbWUiOjE4OTcyOTA4NjE3LCJ2IjowfQ.taBpf9iV0FQikSPk6594pWT444HMAMQn4nXjPkZcg6M");
-    headers = curl_slist_append(headers, "LoginUuid: edf2d4acef7d7d559e10c12d5b5baaa70fdd6b27fb22af35e48c1aa6b4b778bda1647c17962d60e792e4926a08c9dcf0");
-    headers = curl_slist_append(headers, "App-Version: 3");
-    headers = curl_slist_append(headers, "platform: web");
-    headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded");
-    headers = curl_slist_append(headers, "Accept: */*");
-    headers = curl_slist_append(headers, "Origin: https://www.123pan.com");
+    
     curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
 
-    curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, "driveId=0&etag=d41d8cd98f00b204e9800998ecf8427e&fileName=txt111112&parentFileId=0&size=0&type=0");
+    curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, "driveId=0&etag=d41d8cd98f00b204e9800998ecf8427e&fileName=txt112&parentFileId=0&size=0&type=0");
 
     CURLcode ret = curl_easy_perform(hnd);
     printf("code %d\n",ret);
-    //if(!ret){
-        printf(curl_easy_strerror(ret));
-   // }
-    printf("ended\n");
+    if(0!=ret){
+        printf("err:%s\n",curl_easy_strerror(ret));
+        curl_easy_cleanup(curl);
+        ExitProcess(1);
+   }
+    printf("respose:\n");
+    *(str+item)='\0';
+    printf(str);
+    return 0;
 }
