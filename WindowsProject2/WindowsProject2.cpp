966528a6e91e1eab2e8b2118878f69e07f68de4f
x500x <109746464+x500x@users.noreply.github.com>
2024-08-07 10:19:01 +0800

    Add files via upload

--- WindowsProject2/WindowsProject2.cpp
+++ WindowsProject2/WindowsProject2.cpp
@@ -7,28 +7,23 @@
 int main() {
     printf("started\n");
     CURL *hnd = curl_easy_init();
-    int i=0;
-    printf("0\n");++i;
+
     curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "POST");
-    printf("%d\n",i);++i;
-    curl_easy_setopt(hnd, CURLOPT_URL, "https://www.123pan.com/b/api/file/upload_complete");
-    printf("%d\n",i);++i;
+    curl_easy_setopt(hnd, CURLOPT_URL, "https://www.123pan.com/b/api/file/upload_request");
+
     struct curl_slist *headers = NULL;
-    headers = curl_slist_append(headers, "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) alist-client");
-    printf("%d\n",i);++i;
-    headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded");
-    printf("%d\n",i);++i;
+    headers = curl_slist_append(headers, "User-Agent: Mozilla/5.0 (Linux; Android 12; JLH-AN00 Build/HONORJLH-AN00) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.105 Mobile Safari/537.36");
     headers = curl_slist_append(headers, "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjMzNzU5NjcsImlhdCI6MTcyMjc3MTE2NywiaWQiOjE4MzczNzg4NTcsIm1haWwiOiIiLCJuaWNrbmFtZSI6IjE4OTcyOTA4NjE3Iiwic3VwcGVyIjpmYWxzZSwidXNlcm5hbWUiOjE4OTcyOTA4NjE3LCJ2IjowfQ.taBpf9iV0FQikSPk6594pWT444HMAMQn4nXjPkZcg6M");
-    printf("%d\n",i);++i;
-    headers = curl_slist_append(headers, "Platform: open_platform");
-    headers = curl_slist_append(headers, "app-version: 3");
-    printf("%d\n",i);++i;
+    headers = curl_slist_append(headers, "LoginUuid: edf2d4acef7d7d559e10c12d5b5baaa70fdd6b27fb22af35e48c1aa6b4b778bda1647c17962d60e792e4926a08c9dcf0");
+    headers = curl_slist_append(headers, "App-Version: 3");
+    headers = curl_slist_append(headers, "platform: web");
+    headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded");
+    headers = curl_slist_append(headers, "Accept: */*");
+    headers = curl_slist_append(headers, "Origin: https://www.123pan.com");
     curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
-    printf("%d\n",i);++i;
-    curl_easy_setopt(hnd, CURLOPT_COOKIE, "aliyungf_tc=5c841a49275419542a71df1e90017e1c6accd3aef405365eeafd5fa12c9eefc5");
-    printf("%d\n",i);++i;
-    curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, "DriveId=0&duplicate=2&etag=d41d8cd98f00b204e9800998ecf8427e&fileName=txtt1&parentFileId=0&size=0&type=0");
-    printf("%d\n",i);++i;
+
+    curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, "driveId=0&etag=d41d8cd98f00b204e9800998ecf8427e&fileName=txt111112&parentFileId=0&size=0&type=0");
+
     CURLcode ret = curl_easy_perform(hnd);
     printf("ended\n");
 }
