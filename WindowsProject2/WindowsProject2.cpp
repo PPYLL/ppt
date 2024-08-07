aa09080ff45da9c713864be892e03ffed43861f5
x500x <109746464+x500x@users.noreply.github.com>
2024-08-07 10:38:17 +0800

    Update WindowsProject2.cpp

--- WindowsProject2/WindowsProject2.cpp
+++ WindowsProject2/WindowsProject2.cpp
@@ -4,60 +4,50 @@
 #include <stdlib.h>
 #include "curl/curl.h"
 #pragma comment (lib,"libcurl.lib")
-
-struct memory {
-    char* response;
-    size_t size;
-};
-
-static size_t callback(void *data, size_t size, size_t nmemb, void *userp) {
-    size_t realsize = size * nmemb;
-    struct memory *mem = (struct memory *)userp;
-
-    char *ptr = (char*)realloc((void*)mem->response, mem->size + realsize + 1);
-    if(ptr == NULL) {
-        return 0;  /* out of memory! */
-    }
-
-    mem->response = ptr;
-    memcpy(&(mem->response[mem->size]), data, realsize);
-    mem->size += realsize;
-    mem->response[mem->size] = '\0';
-
-    return realsize;
+ 
+size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata)
+{
+    ((std::string*)userdata)->append(ptr, nmemb);
+    return nmemb;
 }
-int main() {
+ 
+ 
+ 
+ 
+int main()
+{
     printf("started\n");
-     /* 1. 初始化 */
+    curl_global_init(CURL_GLOBAL_DEFAULT);
     CURL* curl = curl_easy_init();
-    if (!curl) {
-        printf("curl_easy_init failed\n");
-        return 1;
+    if (curl) 
+    {        
+        curl_easy_setopt(curl, CURLOPT_URL, "https://www.baidu.com");
+ 
+        // no certificate, not verify server certificate
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+ 
+        std::string response_data;
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_data);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        CURLcode res = curl_easy_perform(curl);
+        if (res != CURLE_OK)
+        {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+        }
+        else
+        {
+            long response_code;
+            curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+            printf("response code %ld \n", response_code);
+            printf("response data : \n ");
+            printf(response_data.c_str());
+        }
+ 
+        curl_easy_cleanup(curl);
     }
-
-    struct memory chunk = {0};
-    chunk.response = NULL;
-    chunk.size = 0;
-
-    /* 2. 发送请求 */
-    curl_easy_setopt(curl, CURLOPT_URL, "https://www.baidu.com");
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&chunk);
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-    curl_easy_perform(curl);
-
-    /* 3. 查看请求返回结果 */
-    printf("%s\n", chunk.response);
-
-    /* 4. 清理 */
-    if (chunk.response) {
-        free(chunk.response);
-        chunk.response = NULL;
-        chunk.size = 0;
-    }
-    curl_easy_cleanup(curl);
-    curl = NULL;
-
-    return 0;
+ 
+    curl_global_cleanup();
     printf("ended\n");
+    return 0;
 }
