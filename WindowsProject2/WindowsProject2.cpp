0af6c96857dd7e6f4a38999fee3f9015cacdb278
x500x <109746464+x500x@users.noreply.github.com>
2024-08-07 10:24:25 +0800

    Update WindowsProject2.cpp

--- WindowsProject2/WindowsProject2.cpp
+++ WindowsProject2/WindowsProject2.cpp
@@ -1,29 +1,63 @@
 ﻿#include <windows.h>
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "curl/curl.h"
 #pragma comment (lib,"libcurl.lib")
 
+struct memory {
+    char* response;
+    size_t size;
+};
 
+static size_t callback(void *data, size_t size, size_t nmemb, void *userp) {
+    size_t realsize = size * nmemb;
+    struct memory *mem = (struct memory *)userp;
+
+    char *ptr = (char*)realloc((void*)mem->response, mem->size + realsize + 1);
+    if(ptr == NULL) {
+        return 0;  /* out of memory! */
+    }
+
+    mem->response = ptr;
+    memcpy(&(mem->response[mem->size]), data, realsize);
+    mem->size += realsize;
+    mem->response[mem->size] = '\0';
+
+    return realsize;
+}
 int main() {
     printf("started\n");
-    CURL *hnd = curl_easy_init();
+     /* 1. 初始化 */
+    CURL* curl = curl_easy_init();
+    if (!curl) {
+        printf("curl_easy_init failed\n");
+        return 1;
+    }
 
-    curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_easy_setopt(hnd, CURLOPT_URL, "https://www.123pan.com/b/api/file/upload_request");
+    struct memory chunk = {0};
+    chunk.response = NULL;
+    chunk.size = 0;
 
-    struct curl_slist *headers = NULL;
-    headers = curl_slist_append(headers, "User-Agent: Mozilla/5.0 (Linux; Android 12; JLH-AN00 Build/HONORJLH-AN00) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.105 Mobile Safari/537.36");
-    headers = curl_slist_append(headers, "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjMzNzU5NjcsImlhdCI6MTcyMjc3MTE2NywiaWQiOjE4MzczNzg4NTcsIm1haWwiOiIiLCJuaWNrbmFtZSI6IjE4OTcyOTA4NjE3Iiwic3VwcGVyIjpmYWxzZSwidXNlcm5hbWUiOjE4OTcyOTA4NjE3LCJ2IjowfQ.taBpf9iV0FQikSPk6594pWT444HMAMQn4nXjPkZcg6M");
-    headers = curl_slist_append(headers, "LoginUuid: edf2d4acef7d7d559e10c12d5b5baaa70fdd6b27fb22af35e48c1aa6b4b778bda1647c17962d60e792e4926a08c9dcf0");
-    headers = curl_slist_append(headers, "App-Version: 3");
-    headers = curl_slist_append(headers, "platform: web");
-    headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded");
-    headers = curl_slist_append(headers, "Accept: */*");
-    headers = curl_slist_append(headers, "Origin: https://www.123pan.com");
-    curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
+    /* 2. 发送请求 */
+    curl_easy_setopt(curl, CURLOPT_URL, "https://www.baidu.com");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void*)&chunk);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+    curl_easy_perform(curl);
 
-    curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, "driveId=0&etag=d41d8cd98f00b204e9800998ecf8427e&fileName=txt111112&parentFileId=0&size=0&type=0");
+    /* 3. 查看请求返回结果 */
+    printf("%s\n", chunk.response);
 
-    CURLcode ret = curl_easy_perform(hnd);
+    /* 4. 清理 */
+    if (chunk.response) {
+        free(chunk.response);
+        chunk.response = NULL;
+        chunk.size = 0;
+    }
+    curl_easy_cleanup(curl);
+    curl = NULL;
+
+    return 0;
     printf("ended\n");
 }
