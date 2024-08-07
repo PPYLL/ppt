f6fa2dc58942498242d763236bcfe750b7896294
x500x <109746464+x500x@users.noreply.github.com>
2024-08-07 11:29:38 +0800

    Add files via upload

--- WindowsProject2/WindowsProject2.cpp
+++ WindowsProject2/WindowsProject2.cpp
@@ -1,66 +1,29 @@
 ﻿#include <windows.h>
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include "curl/curl.h"
 #pragma comment (lib,"libcurl.lib")
-#define _CRT_SECURE_NO_WARNINGS 1
-# pragma warning(disable:4996)
 
-bool getUrl(const char *filename)//GET请求
-{
-        CURL *curl;
-        CURLcode res;
-        FILE *fp;
-        if ((fp = fopen(filename, "w")) == NULL)  // 返回结果用文件存储
-                return false;
-        struct curl_slist *headers = NULL;
-        headers = curl_slist_append(headers, "Accept: Agent-007");
-        curl = curl_easy_init();    // 初始化
-        if (curl)
-        {
-                //curl_easy_setopt(curl, CURLOPT_PROXY, "10.99.60.201:8080");// 代理
-                curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);// 改协议头
-                curl_easy_setopt(curl, CURLOPT_URL,"http://www.baidu.com");
-                curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp); //将返回的http头输出到fp>指向的文件,
-                //即为filename文件，而主函数传入的参数是 /tmp/get.html，即为真正保存在get.html文件中
-                
-                curl_easy_setopt(curl, CURLOPT_HEADERDATA, fp); //将返回的html主体数据输出到fp指向的文件
-                res = curl_easy_perform(curl);   // 执行
-                if (res != 0) 
-                {
-                        curl_slist_free_all(headers);  //释放句柄
-                        curl_easy_cleanup(curl);
-                }
-                fclose(fp);
-                return true;
-        }
-}
-/*
-bool postUrl(char *filename)//POST请求
-{
-        CURL *curl;
-        CURLcode res;
-        FILE *fp;
-        if ((fp = fopen(filename, "w")) == NULL)
-                return false;
-        curl = curl_easy_init();
-        if (curl)
-        {
-                curl_easy_setopt(curl, CURLOPT_COOKIEFILE, "/tmp/cookie.txt"); // 指定cookie文件
-                curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "&logintype=uid&u=xieyan&psw=xxx86");    // 指定post内容：用户信息 字段之间&连接，尝试登陆新浪邮箱
-                //curl_easy_setopt(curl, CURLOPT_PROXY, "10.99.60.201:8080");
-                curl_easy_setopt(curl, CURLOPT_URL, " http://mail.sina.com.cn/cgi-bin/login.cgi ");   // 指定url
-                curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
-                res = curl_easy_perform(curl);//执行
-                curl_easy_cleanup(curl);
-        }
-        fclose(fp);
-        return true;
-}
-*/
-int main()
-{
-        getUrl("c:\\codee\\get.html");
-        //postUrl("c:\\codee\\post.html");
+
+int main() {
+    printf("started\n");
+    CURL *hnd = curl_easy_init();
+
+    curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_easy_setopt(hnd, CURLOPT_URL, "https://www.123pan.com/b/api/file/upload_request");
+
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, "User-Agent: Mozilla/5.0 (Linux; Android 12; JLH-AN00 Build/HONORJLH-AN00) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.105 Mobile Safari/537.36");
+    headers = curl_slist_append(headers, "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjMzNzU5NjcsImlhdCI6MTcyMjc3MTE2NywiaWQiOjE4MzczNzg4NTcsIm1haWwiOiIiLCJuaWNrbmFtZSI6IjE4OTcyOTA4NjE3Iiwic3VwcGVyIjpmYWxzZSwidXNlcm5hbWUiOjE4OTcyOTA4NjE3LCJ2IjowfQ.taBpf9iV0FQikSPk6594pWT444HMAMQn4nXjPkZcg6M");
+    headers = curl_slist_append(headers, "LoginUuid: edf2d4acef7d7d559e10c12d5b5baaa70fdd6b27fb22af35e48c1aa6b4b778bda1647c17962d60e792e4926a08c9dcf0");
+    headers = curl_slist_append(headers, "App-Version: 3");
+    headers = curl_slist_append(headers, "platform: web");
+    headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded");
+    headers = curl_slist_append(headers, "Accept: */*");
+    headers = curl_slist_append(headers, "Origin: https://www.123pan.com");
+    curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
+
+    curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, "driveId=0&etag=d41d8cd98f00b204e9800998ecf8427e&fileName=txt111112&parentFileId=0&size=0&type=0");
+
+    CURLcode ret = curl_easy_perform(hnd);
+    printf("ended\n");
 }
