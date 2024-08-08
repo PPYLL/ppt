aec7b558090dc38e991973d479caa0394a4172fd
x500x <109746464+x500x@users.noreply.github.com>
2024-08-08 15:12:43 +0800

    Add files via upload

--- WindowsProject2/WindowsProject2.cpp
+++ WindowsProject2/WindowsProject2.cpp
@@ -1,9 +1,15 @@
-﻿#include <windows.h>
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <windows.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "curl/curl.h"
 #include "cjson/cJSON.h"
 #include "md5/md5.h"
 #pragma comment (lib,"libcurl.lib")
+//禁用安全警告
+# pragma warning(disable:4996)
+size_t item=0;
+
 
 char * md5_hash(char * md5_string,int size)
 {
@@ -25,9 +31,38 @@ char * md5_hash(char * md5_string,int size)
 }
 
 
-int main() {
-    printf("started\n");
-    char FilePath[]=".\\WindowsProject2\\curl\\1";
+struct curl_slist * SetNormalHeaders() {
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, "User-Agent: Mozilla/5.0 (Linux; Android 12; JLH-AN00 Build/HONORJLH-AN00) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.105 Mobile Safari/537.36");
+    headers = curl_slist_append(headers, "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjMzNzU5NjcsImlhdCI6MTcyMjc3MTE2NywiaWQiOjE4MzczNzg4NTcsIm1haWwiOiIiLCJuaWNrbmFtZSI6IjE4OTcyOTA4NjE3Iiwic3VwcGVyIjpmYWxzZSwidXNlcm5hbWUiOjE4OTcyOTA4NjE3LCJ2IjowfQ.taBpf9iV0FQikSPk6594pWT444HMAMQn4nXjPkZcg6M");
+    headers = curl_slist_append(headers, "App-Version: 3");
+    headers = curl_slist_append(headers, "platform: web");
+    headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded");
+    return headers;
+}
+
+
+size_t writeCallback(char *b, size_t size, size_t nitems, void *p)
+{
+    char *str=(char *)p;
+    CopyMemory(str+item,b,nitems);
+    item=1+item+nitems;
+    *(str+item)='\0';
+    return nitems;
+}
+
+char *GetFileName(char *filepath) {
+    int len=strlen(filepath);
+    for(int i = len; i > 0; i--) {
+        if('\\'==*(filepath+i))
+        {
+            return filepath+i+1;
+        }
+    }
+}
+
+
+void PreUpload(CURL *hnd,char *FilePath) {
     HANDLE hFile=CreateFileA(FilePath,GENERIC_READ,
                              0,//可共享读
                              NULL, OPEN_ALWAYS,//打开已经存在的文件
@@ -43,36 +78,68 @@ int main() {
         printf("filepath:%s\n",FilePath);
         ExitProcess(3);
     }
-    printf("filesize:%lld\n",lpFileSize.QuadPart);
     char *filestr=(char *)malloc(lpFileSize.QuadPart);
     ReadFile(hFile,filestr,lpFileSize.QuadPart,NULL,NULL);
-    printf("md5:%s\n",md5_hash(filestr,lpFileSize.QuadPart));
-    /*
-    CURL *hnd = curl_easy_init();
-    curl_easy_setopt(hnd, CURLOPT_SSL_VERIFYPEER, 0);
-    curl_easy_setopt(hnd, CURLOPT_SSL_VERIFYHOST, 0);
-
+    char *datastr=(char *)malloc(1024*2);
+    sprintf_s(datastr,1024*2 ,"driveId=0&etag=%s&fileName=%s&parentFileId=0&size=%lld&type=0", md5_hash(filestr,lpFileSize.QuadPart),GetFileName(FilePath),lpFileSize.QuadPart);
+    printf("\ndata:%s\n\n",datastr);
     curl_easy_setopt(hnd, CURLOPT_CUSTOMREQUEST, "POST");
     curl_easy_setopt(hnd, CURLOPT_URL, "https://www.123pan.com/b/api/file/upload_request");
-
-    struct curl_slist *headers = NULL;
-    headers = curl_slist_append(headers, "User-Agent: Mozilla/5.0 (Linux; Android 12; JLH-AN00 Build/HONORJLH-AN00) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.105 Mobile Safari/537.36");
-    headers = curl_slist_append(headers, "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MjMzNzU5NjcsImlhdCI6MTcyMjc3MTE2NywiaWQiOjE4MzczNzg4NTcsIm1haWwiOiIiLCJuaWNrbmFtZSI6IjE4OTcyOTA4NjE3Iiwic3VwcGVyIjpmYWxzZSwidXNlcm5hbWUiOjE4OTcyOTA4NjE3LCJ2IjowfQ.taBpf9iV0FQikSPk6594pWT444HMAMQn4nXjPkZcg6M");
-    headers = curl_slist_append(headers, "LoginUuid: edf2d4acef7d7d559e10c12d5b5baaa70fdd6b27fb22af35e48c1aa6b4b778bda1647c17962d60e792e4926a08c9dcf0");
-    headers = curl_slist_append(headers, "App-Version: 3");
-    headers = curl_slist_append(headers, "platform: web");
-    headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded");
-
-    headers = curl_slist_append(headers, "Origin: https://www.123pan.com");
-    curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, headers);
-
-    curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, "driveId=0&etag=d41d8cd98f00b204e9800998ecf8427e&fileName=txt111112&parentFileId=0&size=0&type=0");
-
+    curl_easy_setopt(hnd, CURLOPT_POSTFIELDS,datastr);
+    char *respondstr=(char *)malloc(1024*1024);
+    curl_easy_setopt(hnd, CURLOPT_WRITEFUNCTION, writeCallback);
+    curl_easy_setopt(hnd, CURLOPT_WRITEDATA, (void *)respondstr);
     CURLcode ret = curl_easy_perform(hnd);
-    printf("code %d\n",ret);
-    //if(!ret){
-        printf(curl_easy_strerror(ret));
-   // }
-*/
-    printf("ended\n");
+    if(0!=ret) {
+        printf("code %d\n",ret);
+        printf("err:%s\n",curl_easy_strerror(ret));
+        curl_easy_cleanup(hnd);
+        ExitProcess(5);
+    }
+    printf("respose:\n");
+    printf(respondstr);
+
+    cJSON *str_json= cJSON_Parse(respondstr);
+    if (!str_json)
+    {
+        printf("JSON格式错误:%s\n\n", cJSON_GetErrorPtr());
+        cJSON_Delete(str_json);//释放内存
+        ExitProcess(4);
+    }
+    cJSON *data=cJSON_GetObjectItem(str_json, "data");
+    if (!data)
+    {
+        printf("JSON格式错误:%s\n\n", cJSON_GetErrorPtr());
+        cJSON_Delete(str_json);
+        cJSON_Delete(data);
+        ExitProcess(4);
+    }
+    printf("Reuse:%d\n",cJSON_GetObjectItem(data, "Reuse")->valueint);
+    printf("Key:%s\n",cJSON_GetObjectItem(data, "Key")->valuestring);
+    printf("Bucket:%s\n",cJSON_GetObjectItem(data, "Bucket")->valuestring);
+    printf("UploadId:%s\n",cJSON_GetObjectItem(data, "UploadId")->valuestring);
+    printf("SliceSize:%s\n",cJSON_GetObjectItem(data, "SliceSize")->valuestring);
+    printf("StorageNode:%s\n",cJSON_GetObjectItem(data, "StorageNode")->valuestring);
+    cJSON_Delete(str_json);
+    cJSON_Delete(data);
+    // free(str);
+    free(respondstr);
+    free(filestr);
+    free(datastr);
+}
+
+
+
+int main() {
+    printf("start\n\n");
+    CURL *hnd= curl_easy_init();
+    //重要!禁用ssl证书检查
+    curl_easy_setopt(hnd, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_easy_setopt(hnd, CURLOPT_SSL_VERIFYHOST, 0);
+    curl_easy_setopt(hnd, CURLOPT_HTTPHEADER, SetNormalHeaders());
+
+    //curl_easy_setopt(hnd, CURLOPT_POSTFIELDS, "driveId=0&etag=d41d8cd98f00b204e9800998ecf8427e&fileName=txt112&parentFileId=0&size=0&type=0");
+    PreUpload(hnd,(char *)".\\WindowsProject2\\curl\\1");
+
+    return 0;
 }
